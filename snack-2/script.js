1afa1ff14ecad04190aae1d9d21c4279c23b913d
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2024-11-15 14:57:27 +0100

    3 snack complete

--- snack-2/script.js
+++ snack-2/script.js
@@ -6,3 +6,6 @@ const people = [
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+const names = people.filter(element => console.log(element.name));
+// Avremmo potuto anche utilizzare un for each considerando che ci viene chiseto solo di stampare i nomi in console. Se avessimodovuto stampare un nuovo array filter sarebbe stata la soluzione migliore ma ponendo element a .name otteniamo lo stesso risultato.
--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,15 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+
+const incremento = numbers.map((element) => copyArray = element + 1);
+console.log(incremento);
+
+//Oppure avremmo potuto scrivere 
+
+// const incremento = numbers.map((element) => {
+//     let copyArray = element + 1
+//     return copyArray;
+// });
+// console.log(incremento);
